45d20dcb5510689d95a00dd3bd09b11ba7ffa5a5
Abfest <145822383+Abfest@users.noreply.github.com>
2026-04-10 20:35:21 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mytasks-v1';
+const CACHE = 'mytasks-v2';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.svg', '/icon-512.svg'];
 
 self.addEventListener('install', e => {
